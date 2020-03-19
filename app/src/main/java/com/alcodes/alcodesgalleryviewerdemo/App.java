c6ef810cd5ffb151d56149dae782e5cce845278a
KlangChong <kleong153@gmail.com>
2020-03-19 15:24:20 +0800

    Code clean up.

--- app/src/main/java/com/alcodes/alcodesgalleryviewerdemo/App.java
+++ app/src/main/java/com/alcodes/alcodesgalleryviewerdemo/App.java
@@ -8,8 +8,6 @@ import timber.log.Timber;
 
 public class App extends Application {
 
-    // TODO REF: https://proandroiddev.com/your-android-libraries-should-not-ask-an-application-context-51986cc140d4
-
     @Override
     public void onCreate() {
         super.onCreate();
